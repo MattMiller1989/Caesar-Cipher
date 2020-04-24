068aa096b5fb32eb0e4d71621f0832e2296500ad
Matt Miller <mattmiller0989@gmail.com>
2020-04-24 14:49:42 -0500

    Added an initial test

--- spec/Caesar_spec.rb
+++ spec/Caesar_spec.rb
@@ -3,6 +3,10 @@ require_relative '../Caesar_Cipher'
 
 describe '#caesar_shift' do
     it 'returns the shifted string of an object' do
-        expect(caesar_shift("Czggj, Mpwt!", 5)).to eql("What a string!")
+        
+        expect(caesar_shift("Czggj, Mpwt!", 5)).to eql("Hello, Ruby!")
+    end
+    it 'works with a very long string with caps and numbers' do
+        expect(caesar_shift("Mjqqt n fr 30 fsi ymnx xywnsl nx Wjfqqd qtsl fsi btwpx bnym HFUX!!!!!11!!",21)).to eql("Hello i am 30 and this string is Really long and works with CAPS!!!!!11!!")
     end
 end
